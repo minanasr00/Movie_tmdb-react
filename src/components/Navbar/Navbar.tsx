6f712b31210d3ed85124fb49092b662aaf76d335
minanasr00 <62456457+minanasr00@users.noreply.github.com>
2025-06-26 09:53:56 +0300

    finishing navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,23 @@
 import { SiThemoviedatabase } from "react-icons/si";
 import Searchinput from "../common/Searchinput";
-import { useRef } from "react";
+import { useContext, useRef } from "react";
+import { FaSignOutAlt } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/Authcontext";
+import { handleSignOut } from "../../services/firebase/auth";
 
 export default function Navbar() {  
     const searchDivRef = useRef<HTMLDivElement | null>(null)
-    const ulDivRef = useRef<HTMLDivElement | null>(null)
-    return <>
-            
-
-<nav className="bg-white border-gray-200 dark:bg-[#000000cc] mt-5 z-50 border-2 dark:border-blue-600 rounded-4xl max-w-screen-xl mx-auto fixed top-0 right-0 left-0">
+  const ulDivRef = useRef<HTMLDivElement | null>(null)
+  const { user } = useContext(AuthContext)
+  const navigate = useNavigate()
+  return <>
+    {user && <nav className="bg-white border-gray-200 dark:bg-[#000000cc] mt-5 z-50 border-2 dark:border-blue-600 rounded-4xl max-w-screen-xl mx-auto fixed top-0 right-0 left-0">
   <div className="max-w-screen-xl  flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
       <SiThemoviedatabase className="text-white text-5xl"/>
     </a>
-    <div className="flex md:order-2">
+    <div className="flex md:order-2 items-center">
       <button onClick={()=>{searchDivRef.current?.classList.toggle("hidden")}} type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
@@ -27,7 +31,8 @@ export default function Navbar() {
           </svg>
           <span className="sr-only">Search icon</span>
         </div>
-        <Searchinput/>
+              <Searchinput />
+             
       </div>
       <button onClick={()=>{ulDivRef.current?.classList.toggle("hidden")}} data-collapse-toggle="navbar-search" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
         <span className="sr-only">Open main menu</span>
@@ -35,6 +40,18 @@ export default function Navbar() {
           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M1 1h15M1 7h15M1 13h15" />
         </svg>
       </button>
+            <div className="flex text-gray-400 text-md space-x-3 mx-3">
+            {user && <div className="">
+              <FaSignOutAlt onClick={async () => {
+                await handleSignOut()
+                navigate("/login")
+              }} className="text-2xl text-gray-400 hover:text-red-800 " />
+            </div>} 
+              {/* // : <div className="flex space-x-2">
+              //   <Link className="hover:text-green-700 font-bold" to={"/register"}>Sign In</Link>
+              //   <Link className="hover:text-green-700 font-bold" to={"/login"}>Sign Up</Link>
+              // </div> } */}
+      </div>      
     </div>
     <div ref={ulDivRef} className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
       <div className="relative mt-3 md:hidden">
@@ -43,23 +60,26 @@ export default function Navbar() {
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
           </svg>
         </div>
-        <Searchinput/>
+              <Searchinput />
+              
       </div>
-      <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-[#00000024]">
+      <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-[transparent]">
         <li>
-          <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
+          <Link to={"/"} className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</Link>
         </li>
         <li>
-          <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+          <Link to={"/"} className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</Link>
         </li>
         <li>
-          <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+          <Link to={"/"} className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</Link>
+        </li>
+        <li>
+          <Link to={"/"} className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</Link>
         </li>
       </ul>
+            
     </div>
   </div>
-</nav>
-
-
-    </>
+</nav> }
+  </>
 }
--- src/services/firebase/auth.ts
+++ src/services/firebase/auth.ts
@@ -1,3 +1,4 @@
+
 import { auth } from './config'
 import {
     createUserWithEmailAndPassword,
@@ -5,6 +6,7 @@ import {
     type UserCredential,
     GoogleAuthProvider,
     signInWithPopup,
+    signOut,
 } from "firebase/auth";
 
 export async function signIn( email : string , password : string) : Promise<UserCredential> {
@@ -21,3 +23,11 @@ export async function signinWithGoogle() : Promise<UserCredential> {
     return await signInWithPopup(auth, googleProvider)
 }
 
+export const handleSignOut = async () => {
+  try {
+    await signOut(auth);
+    console.log("User signed out successfully");
+  } catch (error) {
+    console.error("Error signing out: ", error);
+  }
+};
